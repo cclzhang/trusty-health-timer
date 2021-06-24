1cd38421feea13aac6b3253873438da89ddec88b
cclzhang <hello@cecilezhang.com>
2021-06-23 21:00:37 -0400

    updated clock so it's active instead of static

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 
 const today = new Date();
@@ -10,12 +11,24 @@ console.log(today.toLocaleTimeString('en-GB'));
 console.log(today.toLocaleTimeString());
 
 function App() {
+  const [clockHr, setClockHr] = useState(new Date().getHours());
+  const [clockMin, setClockMin] = useState(new Date().getMinutes());
+
+  useEffect(() => {
+    let interval = null;
+    interval = setInterval(() =>{
+      setClockHr(new Date().getHours());
+      setClockMin(new Date().getMinutes());
+      // console.log(new Date().getSeconds());
+    }, 1000)
+    return (()=> clearInterval(interval))
+  })
   return (
     <div className="App">
       <Header />
       <Main />
       <aside>
-        <p className="time">{currentTime}</p>
+        <p className="time">{`${clockHr}:${clockMin}`}</p>
       </aside>
       <footer>
         <p>&copy; 2021 <a href="http://cecilezhang.com">xixi</a></p>
