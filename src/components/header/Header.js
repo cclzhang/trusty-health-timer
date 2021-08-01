78a97473b137dbaa4e0499f658b70a4c1a68b354
cclzhang <hello@cecilezhang.com>
2021-07-31 23:59:07 -0400

    added css font styles and icon updates

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,6 +6,8 @@ import Info from './info/Info';
 import Settings from './settings/Settings';
 
 import './Header.css';
+import logo from '../../assets/images/lotus.png';
+import { LightBulbIcon, GearIcon } from '@primer/octicons-react';
 
 
 
@@ -27,19 +29,22 @@ const Header = () => {
   return (
     <header>
       <h1>
-        <a href="." className="logo">Healthy Pomo</a>
+        <a href="." className="logo">
+          <img src={logo} alt="lotus logo" />
+          healthpomo
+        </a>
       </h1>
       <nav>
         <ul>
           <li>
             <button className="infoBtn" name="infoBtn" onClick={
               SettingsComponent ? toggleInfoAndSettings : toggleInfoVisibility
-            }>i</button>
+            }><LightBulbIcon size={16} /></button>
           </li>
           <li>
             <button className="settingsBtn" name="settingsBtn" onClick={
               InfoComponent ? toggleInfoAndSettings : toggleSettingsVisibility
-            }>s</button>
+            }><GearIcon size={16} /></button>
           </li>
         </ul>
       </nav>
